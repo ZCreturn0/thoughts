cd665a3c8419aad723f0813129fa4240806acea1
ZCreturn0 <237350543@qq.com>
2019-12-01 21:50:06 +0800

    hasClass实现

--- 简历模版/js/index.js
+++ 简历模版/js/index.js
@@ -1,25 +1,29 @@
 // 工具函数对象
+// 所有方法均为静态方法
 class Tools {
-    static tool = null;
     constructor() {
-        
+        if(new.target === Tools) {
+            throw new ReferenceError('Tools 类无需创建对象!');
+        }
     }
-    // 单例
-    static getInstance() {
-        return Tools.tool || (Tools.tool = new Tools());
+    /**
+     * @description 判断元素是否含有指定 class
+     * @param {object} el 元素
+     * @param {string} className 类名
+     * @returns {boolean}
+     */
+    static hasClass(el, className) {
+        return !!~[].indexOf.call(el.classList, className);
     }
 }
+
+;(function() {
+    // console.log(DATA);
+    operationBtnEvents();
+})();
 
 // 操作按钮注册事件
 function operationBtnEvents() {
     let pauseBtn = document.getElementsByClassName('pause')[0];
     let speedUp = document.getElementsByClassName('speedUp')[0];
 }
-
-// 入口函数
-function run() {
-    // console.log(DATA);
-    console.log(Tools.getInstance());
-}
-
-run();
