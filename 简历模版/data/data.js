123deb23382b888ce0d810bbaf85946415a31c5a
zc_return0 <237350543@qq.com>
2019-12-02 10:51:38 +0800

    纠正错别字

--- 简历模版/data/data.js
+++ 简历模版/data/data.js
@@ -4,6 +4,6 @@ const DATA = {
         '这是 ZCreturn0 开发的的一款简历模版,灵感来自知乎的某篇文章.本项目地址: https://github.com/ZCreturn0/thoughts/tree/master/%E7%AE%80%E5%8E%86%E6%A8%A1%E7%89%88. 觉得不错请点个 star!!',
         '改变本文件内容换成你想展示的内容,introduce 中每个元素为一段.',
         'introduce 完后会进入编写代码展示简历环节.',
-        '可根据需求更改代码,该有注释的地方都谢了注释.'
+        '可根据需求更改代码,该有注释的地方都写了注释.'
     ]
 };
